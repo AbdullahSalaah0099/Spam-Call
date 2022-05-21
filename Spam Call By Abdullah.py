e00c30c14606f99d76be940b48486361aa14f803
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-05-21 10:15:42 +0200

    Update Spam Call By Abdullah.py

--- Spam Call By Abdullah.py
+++ Spam Call By Abdullah.py
@@ -9,18 +9,18 @@ print ()
 print ('\033[1;92mclick on this link to get password👇')
 #sleep (0.1)
 print ()
-link1="\033[1;93m https://miklpro.com/SWNkHNK1"
+link1="\033[1;93m https://miklpro.com/iYOyKpuc"
 print (link1)
 #sleep (1)
 print ()
 password=input ('\033[1;92m》Enter Password Script :  \033[1;96m')
 sleep (1)
 
-rrr=requests.get('https://pastelink.net/trs4u411').text
+'''rrr=requests.get('https://pastelink.net/trs4u411').text
 soup=BS(rrr,'html.parser')
-lxc=(soup.find('div',{'class':'body-display'})).text
+lxc=(soup.find('div',{'class':'body-display'})).text'''
 
-if password in lxc:
+if password =="Abdullah8010:
     print ()
     print ('\033[1;96m》True Password《')
     sleep (1)
